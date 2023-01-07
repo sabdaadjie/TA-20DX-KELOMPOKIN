fb540cd5459ccbf44263ccdce3b42d2422406e81
sabdaadjie <sabdaadjie@gmail.com>
2023-01-07 16:10:13 +0700

    master : frontend tampilan website untuk user (alamat toko)

--- application/views/pages_user/index.php
+++ application/views/pages_user/index.php
@@ -102,7 +102,168 @@
 	<!-- end price list -->
 
 
-	
+	<!-- alamat toko sesion -->
+	<div class="block__73694 site-section border-top" id="alamat-toko-section">
+		<div class="container">
+			<div class="row d-flex no-gutters align-items-stretch">
+				<div class="col-12 col-lg-6 block__73422 order-lg-2" data-aos="fade-left"> <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3971.946954619355!2d105.25608971476537!3d-5.425030496065447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e40da49085b072b%3A0xded2229dc6987030!2sBestnet%20Computer!5e0!3m2!1sid!2sid!4v1673001158483!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="w-100" height="400" allowfullscreen="" loading="lazy"></iframe>
+				</div>
+
+				<div class="col-lg-5 mr-auto p-lg-5 mt-4 mt-lg-0 order-lg-1" data-aos="fade-right">
+					<h2 class="mb-4 text-black">BESTNET COMPUTER</h2>
+					<h4 class="text-primary">Alamat Toko</h4>
+					<p>Jl. M. Husni Thamrin No.177, Gotong Royong, Kec. Tj. Karang Pusat, Kota Bandar Lampung, Lampung 35214</p>
+					<p>Buka
+						<br>Senin - Sabtu
+						<br>Pukul 08:00 - 16:30
+					</p>
+					<h4 class="text-primary">Melayani</h4>
+					<ul class="ul-check primary list-unstyled mt-5">
+						<li>Jual Komputer,Laptop,Printer & Networking</li>
+						<li>Service Komputer & Printer</li>
+						<li>Installasi CCTV</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end alamat toko -->
+
+
+
+	<!-- Contact sesion -->
+	<div class="site-section bg-light" id="contact-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 text-center mb-5" data-aos="fade-up">
+					<div class="block-heading-1">
+
+						<h2>Contact Us</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-6 mb-5" data-aos="fade-up" data-aos-delay="100">
+					<form action="#" method="post">
+						<div class="form-group row">
+							<div class="col-md-6 mb-4 mb-lg-0">
+								<input type="text" class="form-control" placeholder="First name">
+							</div>
+							<div class="col-md-6">
+								<input type="text" class="form-control" placeholder="First name">
+							</div>
+						</div>
+
+						<div class="form-group row">
+							<div class="col-md-12">
+								<input type="text" class="form-control" placeholder="Email address">
+							</div>
+						</div>
+
+						<div class="form-group row">
+							<div class="col-md-12">
+								<textarea name="" id="" class="form-control" placeholder="Write your message." cols="30" rows="10"></textarea>
+							</div>
+						</div>
+						<div class="form-group row">
+							<div class="col-md-6 mr-auto">
+								<input type="submit" class="btn btn-block btn-primary text-white py-3 px-5" value="Send Message">
+							</div>
+						</div>
+					</form>
+				</div>
+				<div class="col-lg-4 ml-auto" data-aos="fade-up" data-aos-delay="200">
+					<div class="bg-white p-3 p-md-5">
+						<h3 class="text-black mb-4">Contact Info</h3>
+						<ul class="list-unstyled footer-link">
+							<li class="d-block mb-3">
+							<li class="d-block mb-3"><span class="d-block text-black">Phone:</span><span>0721-262666</span></li>
+							<li class="d-block mb-3"><span class="d-block text-black">Telp & Whatsapp:</span><span>082186948453</span></li>
+							<li class="d-block mb-3"><span class="d-block text-black">Email:</span><span>bestnetcomputer3@gmail.com</span></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end contact -->
+
+	<!-- About sesion -->
+	<div class="site-section py-5" id="about-section">
+		<div class="container">
+			<div class="row justify-content-center text-center mb-5">
+				<div class="col-lg-4 mb-5 mb-lg-0">
+					<div class="block-heading-1" data-aos="fade-right">
+						<h2>About</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="mb-5 d-flex blog-entry" data-aos="fade-right">
+						<div class="blog-excerpt">
+							<h2 class="mb-3 text-primary">BESTNET COMPUTER</h2>
+
+							<p>CV. Bina Era Satriatama Netwrok atau di singkat Bestnet adalah perusahaan suplier peralatan IT, termasuk
+								didalamnya semua barang dan jasa yang berkaitan dengan teknologi informasi.
+								Kami melayani perusahaan-perusahaan, kantor, pabrik, supermarket, pendidikan,
+								bank, koperasi, asuransi, hingga home user atau pengguna di rumah. Kami
+								membantu memberikan solusi dibidang IT, menyediakan barang-barang
+								Teknologi Informasi yang berkualitas, bergaransi, dengan harga terbaik.
+								<br>
+								<br>
+								Nama Perusahaan : CV Bina Era Satriatama Network(BestNet Computer)
+								<br>
+								Website : www.bestnetcomputer.com
+								<br>
+								Alamat : Jl. Diponegoro No. 177 - Gotong Royong Bandarlampung
+								<br>
+								Telepon : 0721 262666 / 7490008
+								<br>
+								Faximile : 0721 262666
+								<br>
+								Tahun Berdiri : 2004
+								<br>
+								Pemilik : Budiono
+								<br>
+								Line Of Bussines : Best IT Solution, Sales, Repair and Maintenance
+								<br>
+								<br>
+								Semenjak berdiri tahun 2004, hingga saat ini, kami telah dipercaya untuk
+								bekerja sama dengan berbagai vendor / produsen produk-produk komputer. Para
+								vendor tersebut memberikan dukungan sepenuhnya terhadap produk-produk yang
+								dijual. Dukungan yang diberikan berupa garansi terhadap produk, maupun harga
+								yang bersaing.
+								<br>
+								<br>
+								Produk-produk yang di jual antara lain:
+								<br>
+								1. Server: HP Compaq, IBM, Dell, Acer.
+								<br>
+								2. Desktop PC dan Workstation: HP, Compaq, Acer, IBM, Dell, Ion, Zyrex.
+								<br>
+								3. Notebook: HP Compaq Presario, Toshiba, Acer, Sony, Axioo, Asus.
+								<br>
+								4. Projector: Toshiba, BenQ, Sony, Acer.
+								<br>
+								5. Printer Inkjet, Canon, Epson, HP, Lexmar, OKI, IBM, Samsung.
+								<br>
+								6. Produk Networking / Jaringan dan Telephon: Linksys, Cisco, 3COM, DLink,
+								SureCom, Sena, Mikrotik, Bluelink, TP-link.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="mb-5 d-flex blog-entry" data-aos="fade-right">
+						<a href="#" class="blog-about"><img src="images/bestnet.jpg" alt="" class="img-fluid"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+	<!-- end about -->
 
 	<!--  footer start -->
 	<?php include "footer.php"; ?>
